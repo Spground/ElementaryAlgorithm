cf21025adf70415686dd19bd0e48a7a9ea280201
wujie <958340585@qq.com>
2015-10-24 17:34:54 +0800

    add a new one

---
+++ src/cn/edu/dlut/wujie/CountPrime.java
@@ -0,0 +1,29 @@
+package cn.edu.dlut.wujie;
+
+public class CountPrime {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	//Count the number of prime numbers less than a non-negative number, n
+	private static int countPrime(int n){
+		int count=0;
+        boolean[] nums = new boolean[n];
+        for(int i=2; i<nums.length; i++){
+            if(!nums[i]){
+                count++;
+                //key point:set nums[k * i] as false,because these numbers no more be prime
+                //which can reduce the inner for-loop times 
+                for(int j=2*i; j<nums.length; j=j+i){
+                        nums[j] = true;
+                }
+            }
+        }
+        return count;
+	}
+}
